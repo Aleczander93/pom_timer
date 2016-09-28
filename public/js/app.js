b8929c46b66700ffd806e9a547330bba4c6ae53f
aleczander <afigueroa81@ufl.edu>
2016-09-28 19:41:49 -0400

    added code to appjs to create seconds timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,39 @@
 (function() {
   //initialize variables
-  var startbutton= $("#start");
+  var startButton= $("#start");
+  var seconds = $("#seconds");
 
   //main functionality
-  startbutton.on("click", countdown);
+  startButton.on("click", countdown);
 
   //function definition
   function countdown(){
-    alert("It's the final countdown!!!");
+    var secondsText = seconds.text ();
+    var secondsTextAsNumber = parseInt(secondsText);
+    if (secondsTextAsNumber === 0) {
+      seconds.text ("59");
 
+      //then change seconds text to 59
+  } else {
+
+    var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+    var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+    seconds.text(padSecondsTextAsNumber);
   }
+    // var secondsValue = parseInt(seconds.text());
+    // console.log(secondsValue);
+    // seconds.text(pad(secondsValue - 1));
+    //transforms letter to text
+  }
+
+    function pad (num){
+        if(num < 10){
+          //spit out number with a leading 0
+          return "0" + num;
+
+      } else {
+        //spit out the original number
+        return num;
+      }
+    }
 }());
